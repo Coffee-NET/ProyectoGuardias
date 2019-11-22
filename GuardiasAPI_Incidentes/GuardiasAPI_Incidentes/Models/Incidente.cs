6945bbe4c0bf976c7d65deb68feac2707a1f44a8
Picciani <luca.f.picciani@accenture.com>
2019-11-22 16:36:36 -0300

    commit inicial

---
+++ GuardiasAPI_Incidentes/GuardiasAPI_Incidentes/Models/Incidente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardiasAPI_Incidentes.Models
+{
+    public class Incidente
+    {
+        public int ID { get; set; }
+        public int ID_Job { get; set; }
+        public string descripcion { get; set; }
+        public string solucion { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
