936198f8b91dc211eeca0e3b1ce56da61aaf5bc7
PabloLubrano <pablolubrano@gmail.com>
2019-11-25 17:01:24 -0300

    Cambios API Jobs

---
+++ GuardiasAPI_Jobs/GuardiasAPI.Jobs.Models/Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GuardiasAPI.Jobs.Models
+{
+    public class Job
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Sigla { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+        public string PreRequisitos { get; set; }
+
+        public string PostRequisitos { get; set; }
+
+        [Required]
+        public string Horario { get; set; }
+    }
+}
